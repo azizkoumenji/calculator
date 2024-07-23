03414c954f97e89b6e3ee33fec6a033784140b06
Aziz Koumenji <azizkoumenji@protonmail.com>
2024-07-23 16:59:35 +0100

    Added some formatting

--- src/app.js
+++ src/app.js
@@ -5,60 +5,71 @@ class App extends React.Component {
     super(props);
     this.state = {
       equation: "",
-      result: "0"
+      result: "0",
     };
-    this.handleclick = this.handleclick.bind(this)
+    this.handleclick = this.handleclick.bind(this);
   }
-  
+
   handleclick(event) {
-    console.log(event.target.id)
+    console.log(event.target.id);
     if (event.target.id == "=") {
-      let newResult = eval(this.state.equation).toFixed(3)
-      console.log(newResult)
-      this.setState(
-        {
-          equation: "",
-          result: newResult
-        }
-      )
+      let newResult = eval(this.state.equation).toFixed(3);
+      console.log(newResult);
+      this.setState({
+        equation: "",
+        result: newResult,
+      });
     } else if (event.target.id == "AC") {
-      this.setState(
-        {
-          equation: "",
-          result: "0"
-        }
-      )
+      this.setState({
+        equation: "",
+        result: "0",
+      });
     } else {
       if (event.target.id == "0" && this.state.equation.at(-1) == "0") {
-
       } else {
-      let newEquation = this.state.equation + event.target.id
-      this.setState(
-        {
+        let newEquation = this.state.equation + event.target.id;
+        this.setState({
           equation: newEquation,
-          result: ""
-        }
-      )
-    }
+          result: "",
+        });
+      }
     }
   }
 
   render() {
-    let symbols = ["AC","/", "*", "-","7", "8", "9", "+","4", "5", "6", "=","1", "2", "3", "."];
+    let symbols = [
+      "AC",
+      "/",
+      "*",
+      "-",
+      "7",
+      "8",
+      "9",
+      "+",
+      "4",
+      "5",
+      "6",
+      "=",
+      "1",
+      "2",
+      "3",
+      ".",
+    ];
     let buttons = [];
     let j = 0;
     let k = 0;
     for (let i = 0; i < 4; i++) {
       let columns = [];
       for (j; j < 4 + k; j++) {
-        columns.push(<button className="col" onClick={this.handleclick} id={symbols[j]}>{symbols[j]}</button>)
+        columns.push(
+          <button className="col" onClick={this.handleclick} id={symbols[j]}>
+            {symbols[j]}
+          </button>
+        );
       }
       k = j;
-      buttons.push(
-        <div className="row">{columns}</div>
-      )
+      buttons.push(<div className="row">{columns}</div>);
     }
-    console.log(buttons)
     return (
       <div className="calculator d-flex align-items-center flex-column">
         <div className="calculator-display d-flex flex-column">
@@ -67,7 +78,11 @@ class App extends React.Component {
         </div>
         <div className="calculator-keys d-flex flex-column justify-content-center">
           {buttons}
-          <div className="row"><button className="zero" onClick={this.handleclick} id="0">0</button></div>
+          <div className="row">
+            <button className="zero" onClick={this.handleclick} id="0">
+              0
+            </button>
+          </div>
         </div>
       </div>
     );
